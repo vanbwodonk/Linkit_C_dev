b7890c65f57b82d9bb4f9fcbb8f9d7a6f72348e7
Arif Darmawan <arif.pens@gmail.com>
2018-02-20 14:22:49 +0700

    more hello

--- Software/mraa_hello/mraa_hello.c
+++ Software/mraa_hello/mraa_hello.c
@@ -2,6 +2,7 @@
 #include "mraa/common.h"
 int main(int argc, char** argv)
 {
+    mraa_platform_t platform = mraa_get_platform_type();
     const char* board_name = mraa_get_platform_name();
     fprintf(stdout, "hello mraa\n Version: %s\n Running on %s\n", mraa_get_version(), board_name);
     mraa_deinit();
