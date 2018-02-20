cf9c12800b3a2031cb81dacd8ce6da226ade396b
Arif Darmawan <arif.pens@gmail.com>
2018-02-20 11:00:36 +0700

    add linkit MT7688 SDK

---
+++ SDK/build_dir/target-mipsel_24kec+dsp_uClibc-0.9.33.2/linux-ramips_mt7688/linux-3.18.21/include/generated/compile.h
@@ -0,0 +1,6 @@
+/* This file is auto generated, version 2 */
+#define UTS_MACHINE "mips"
+#define UTS_VERSION "#2 Sun Nov 22 12:41:41 CET 2015"
+#define LINUX_COMPILE_BY "blogic"
+#define LINUX_COMPILE_HOST "debian"
+#define LINUX_COMPILER "gcc version 4.8.3 (OpenWrt/Linaro GCC 4.8-2014.04 r47072) "
